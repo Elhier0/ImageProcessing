dfc635df3f62aaf1f35cd5e39572e7e22dcee96b
Furkan OZKAYA <114191377+Elhier0@users.noreply.github.com>
2024-06-02 02:59:27 +0300

    Add files via upload
       - Convert images to grayscale
       - Rotate images by a specified angle
       - Generate and save histogram of pixel values
       - Combine multiple images into one

---
+++ Basic Image Processing with PIL.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+#FURKAN ÖZKAYA 200316060
+
+
+image = Image.open("C:/Users/furkan\Desktop\paris.png")
+
+gray_image = image.convert("L")
+
+rotated_image = image.rotate(60)
+
+histogram = image.histogram()
+
+plt.bar(range(256), histogram[:256], color='blue', alpha=0.7)
+plt.xlabel('Pixel Değeri')
+plt.ylabel('Pixel Sayısı')
+plt.title('Histogram')
+plt.savefig('histogram.png')
+plt.close()
+
+new_image = Image.new('RGB', (image.width * 2, image.height * 2), color='white')
+
+new_image.paste(image, (0, 0))
+new_image.paste(gray_image, (image.width, 0))
+new_image.paste(rotated_image, (0, image.height))
+new_image.paste(Image.open('histogram.png'), (image.width, image.height))
+
+new_image.save('combined_img_py.png')
+
